7fe788c05d9919c28f93e7a4fb9827e7825f2c70
Clare Jolly <clarejollyuk@gmail.com>
2018-10-08 17:28:52 +0100

    entry station

--- lib/card.rb
+++ lib/card.rb
@@ -1,14 +1,14 @@
 class Card
 
   attr_reader :balance
-  attr_accessor :journey
+  attr_reader :entry_station
 
   LIMIT = 90
   MINIMUM_FARE = 1
 
   def initialize(balance = 0)
     @balance = balance
-    @journey = false
+    @entry_station = nil
   end
 
   def top_up(money)
@@ -16,18 +16,18 @@ class Card
     @balance += money
   end
 
-  def touch_in
+  def touch_in(station)
     fail "You need a minimum balance of £#{MINIMUM_FARE} to enter barrier." if balance_low
-    self.journey=(true)
+    @entry_station = station
   end
 
   def touch_out
     deduct(MINIMUM_FARE)
-    self.journey=(false)
+    @entry_station = nil
   end
 
   def in_journey?
-    journey
+    !!entry_station
   end
 
   private
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,6 +2,8 @@ require 'card'
 
 describe Card do
 
+  let(:station){ double :station }
+
   before(:each) do
     @card = Card.new(10)
   end
@@ -24,22 +26,33 @@ describe Card do
   end
 
   it 'starts a journey' do
-    @card.touch_in
+    @card.touch_in(station)
     expect(@card.in_journey?).to eq true
   end
 
   it 'ends a journey' do
-    @card.touch_in
+    @card.touch_in(station)
     @card.touch_out
     expect(@card.in_journey?).to eq false
   end
 
   it "Doesn't allow you through the barrier if balance less than £1" do
     card = Card.new(0.99)
-    expect { card.touch_in }.to raise_error "You need a minimum balance of £#{Card::MINIMUM_FARE} to enter barrier."
+    expect { card.touch_in(station) }.to raise_error "You need a minimum balance of £#{Card::MINIMUM_FARE} to enter barrier."
   end
 
   it "deducts the journey cost from balance" do
     expect {@card.touch_out}.to change{@card.balance}.by(-(Card::MINIMUM_FARE))
+  end
+
+  it "Remembers the entry station" do
+    @card.touch_in(station)
+    expect(@card.entry_station).to eq station
+  end
+
+  it "clears the entry station" do
+    @card.touch_in(station)
+    @card.touch_out
+    expect(@card.entry_station).to eq nil
   end
 end
